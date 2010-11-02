f4c3fcab2b4c67bdcb6a12034086eb193764d068
guirreri (PhD ) <sergioguirreri@gmail.com>
2010-11-02 17:11:25 +0000

    Many thanks to Joshua Ulrich  to have extended the YieldCurve functions to the objects of the xts package
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/yield-curve@36 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Nelson.Siegel.R
+++ pkg/R/Nelson.Siegel.R
@@ -1,28 +1,16 @@
 `Nelson.Siegel` <-
 function( rate, maturity, MidTau)
   {
+    rate <- try.xts(rate,error=as.matrix)
+    
     a1 <- mean(c(MidTau[1],MidTau[2]))
     a2 <- mean(c(MidTau[1],MidTau[3]))
     a3 <- mean(c(MidTau[2],MidTau[3]))
+
     lambdaValues <- c( MidTau,a1, a2, a3)
-    if(is.vector(rate)) rate <- matrix(rate, 1, length(maturity))
-    if(is.data.frame(rate)) rate <- data.matrix(rate)
-    factorBeta1 <- function( lambda, maturity ) {
-     ( 1-exp( -lambda * maturity ) ) / ( lambda * maturity ) 
-    }
-    factorBeta2 <- function(lambda, maturity ) {
-     ( 1-exp( -lambda * maturity ) ) / ( lambda * maturity ) - exp( -lambda * maturity ) 
-    }
-    beta.estimator <- function( rate, maturity, lambda )
-    {
-      beta <- lm( rate ~ 1 + factorBeta1(lambda,maturity) + factorBeta2(lambda,maturity) )
-      betaPar <- coef(beta)
-      NaValues <- na.omit(betaPar)
-      if( length(NaValues)<3 ) betaPar <- c(0,0,0)
-      names(betaPar) <- c("beta_0", "beta_1", "beta_2")
-      EstResults <- list(Par=betaPar, Res=resid(beta))
-      return(EstResults)
-     }
+    #if(is.vector(rate)) rate <- matrix(rate, 1, length(maturity))
+    #if(is.data.frame(rate)) rate <- data.matrix(rate)
+
     FinalResults <- matrix(0, nrow(rate), 4)
     colnames( FinalResults ) <- c("beta_0","beta_1","beta_2","lambda")
     j <- 1
@@ -32,8 +20,9 @@ function( rate, maturity, MidTau)
         colnames( InterResults ) <- c("beta0","beta1","beta2","lambda","SSR")
         for( i in 1:length(lambdaValues))
           {
-            lambdaTemp <- optimize(factorBeta2,interval=c(0.001,1),maturity=lambdaValues[i],maximum=TRUE)$maximum
-            InterEstimation<- beta.estimator( rate[j,], maturity, lambda=lambdaTemp)
+            lambdaTemp <- optimize(.factorBeta2,interval=c(0.001,1),
+              maturity=lambdaValues[i],maximum=TRUE)$maximum
+            InterEstimation <- .NS.estimator(rate[j,], maturity, lambdaTemp)
             BetaCoef <- InterEstimation$Par
 	    if( BetaCoef[1]>0 & BetaCoef[1]<20)
               {
@@ -41,14 +30,12 @@ function( rate, maturity, MidTau)
                 InterResults[i,] <- c(BetaCoef, lambdaTemp, SSR)
               } else
             {
-              InterResults[i,] <- c(BetaCoef,lambdaValues[i],100000)
+              InterResults[i,] <- c(BetaCoef,lambdaValues[i],1e+5)
             } 
           }
         BestRow <- which.min(InterResults[,5])
         FinalResults[j,] <- InterResults[BestRow,1:4]
         j <- j+1
       }
-    FinalResults <- as.data.frame(FinalResults)
-    return( FinalResults )
+    reclass( FinalResults, rate )
   }
-

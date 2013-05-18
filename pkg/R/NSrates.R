c3d97c0a10c3bfef60f9f549ce80b9a95f4ff045
guirreri (PhD ) <sergioguirreri@gmail.com>
2013-05-18 15:47:22 +0000

    Solve Bug on NSrates
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/yield-curve@55 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/NSrates.R
+++ pkg/R/NSrates.R
@@ -1,14 +1,13 @@
 `NSrates` <- function ( Coeff, maturity )
   {
-    Curve <- xts(matrix( 0, nrow(Coeff), length(maturity) ), order.by=time(Coeff))
+    Curve <- try.xts(matrix(0, nrow(Coeff), length(maturity)), order.by = time(Coeff), error=as.matrix)
     colnames(Curve) <- make.names(maturity)
-    Coeff <- as.matrix( Coeff )
-    
-    for(i in 1:nrow(Curve))
-      {
-        Curve[i,] <- as.numeric(Coeff[i,1]) * rep(1, length(maturity)) +
-          as.numeric(Coeff[i,2]) * as.numeric(.factorBeta1(Coeff[i,4], maturity) ) +
-          as.numeric(Coeff[i,3]) * as.numeric(.factorBeta2(Coeff[i,4], maturity ))
-      }
-    return( Curve )
-  } 
+    Coeff <- as.matrix(Coeff)
+    for (i in 1:nrow(Curve)) {
+        Curve[i, ] <- as.numeric(Coeff[i, 1]) * rep(1, length(maturity)) +
+            as.numeric(Coeff[i, 2]) * as.numeric(.factorBeta1(Coeff[i,
+                4], maturity)) + as.numeric(Coeff[i, 3]) * as.numeric(.factorBeta2(Coeff[i,
+            4], maturity))
+    }
+    return(Curve)
+}
